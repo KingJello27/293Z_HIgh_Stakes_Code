d1d78c885de1e3bcef0bb282656efbd2aa2e8da8
Ishan Voleti <ishan.voleti@gmail.com>
2024-11-17 16:55:42 -0500

    Updated Comp Auton to make 1/2 AWP not go back

--- src/comp/autonomous.cpp
+++ src/comp/autonomous.cpp
@@ -87,7 +87,7 @@ void autonomous() {
             chassis.wait_drive();
             chassis.set_turn_pid(0,100);
             chassis.wait_drive();
-            chassis.set_drive_pid(12.5, 80);
+            chassis.set_drive_pid(11.5, 80);
             chassis.wait_drive();
             doinker.set_value(true);
             pros::delay(250);
@@ -208,8 +208,8 @@ void autonomous() {
             pros::delay(250);
             tilter.set_value(false);
             pros::delay(1000);
-            chassis.set_drive_pid(2.5,80);
-            chassis.wait_drive();
+            // chassis.set_drive_pid(2.5,80);
+            // chassis.wait_drive();
             intake.move_velocity(0);
             
             
@@ -219,7 +219,7 @@ void autonomous() {
             //Blue Left 1/2 AWP
             
             //score on alliance stake
-            chassis.set_drive_pid(1.8,80);
+            chassis.set_drive_pid(1.95,80);
             chassis.wait_drive();
             chassis.set_turn_pid(-90,100);
             chassis.wait_drive();
@@ -257,7 +257,7 @@ void autonomous() {
             pros::delay(500);
 
             //grab second mobile goal
-            chassis.set_drive_pid(6,100);
+            chassis.set_drive_pid(5,100);
             chassis.wait_drive();
             chassis.set_turn_pid(90,100);
             chassis.wait_drive();
